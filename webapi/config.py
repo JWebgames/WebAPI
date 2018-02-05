b86305f33276704e72948765f91b76a8ad25025c
Julien <Julien.castiaux@gmail.com>
2018-02-05 01:03:12 +0100

    config done

--- webapi/config.py
+++ webapi/config.py
@@ -1,10 +1,12 @@
-from collections import ChainMap
-from typing import NamedTuple, Optional
+from collections import ChainMap, namedtuple
+from typing import NamedTuple, Optional, Union
 from argparse import ArgumentParser
 from pathlib import Path
+from os import environ
+from sys import argv
 from yaml import load as yaml_load
-from exceptions import ConfigOptionTypeError,
-                       ConfigUnknownOptionError,
+from exceptions import ConfigOptionTypeError,\
+                       ConfigUnknownOptionError,\
                        ConfigMissingOptionError
 
 
@@ -13,12 +15,9 @@ class WebAPIConfig(NamedTuple):
     PORT: int
     JWT_SECRET: str
 
-    def __str__(self):
-        return "webapi"
-
 
 class PostgresConfig(NamedTuple):
-    DNS: Optional[str] = None
+    DSN: Optional[str] = None
     HOST: Optional[str] = None
     PORT: Optional[int] = None
     USER: Optional[str] = None
@@ -26,46 +25,92 @@ class PostgresConfig(NamedTuple):
     PASSWORD: Optional[str] = None
     SSL: Optional[bool] = None
 
-    def __str__(self):
-        return "postgres"
 
+def cast(typ, val):
+    return real_type(typ)(val)
+
+
+def real_type(typ):
+    if typ.__class__ in [Union.__class__, Optional.__class__]:
+        return typ.__args__[0]
+    else:
+        return typ
+
+
+Triple = namedtuple("Triple", ["name", "prefix", "block"])
+triples = [
+    Triple("webapi", "WG_API_", WebAPIConfig),
+    Triple("postgres", "PG", PostgresConfig)
+]
 
 webapi: WebAPIConfig
 postgres: PostgresConfig
 def load():
-    config = ChainMap(load_from_file(), load_from_environ(), load_from_cli())
-
-    validate_config(WebAPIConfig, config["webapi"])
-    validate_config(PostgresConfig, config["postgres"])
-
-    global webapi, postgres
-    webapi = WebAPIConfig(**config["webapi"])
-    postgres = PostgresConfig(**config["postgres"])
+    cli = load_from_cli()
+    env = load_from_environ()
+    yml = load_from_file()
+    
+    for name, _, block in triples:
+        config = ChainMap(cli.get(name, {}), env.get(name, {}), yml.get(name, {}))
+        validate_config(name, block, config)
+        globals()[name] = block(**config)
 
 
 def load_from_file():
     with Path("config.yml").open() as yaml_file:
-        return yaml_load(yaml_file)
+        yaml_config = yaml_load(yaml_file)
+
+    for name, _, block in triples:
+        for key in set(block._fields) & set(yaml_config[name]):
+            if yaml_config[name][key] is not None:
+                yaml_config[name][key] = cast(block._field_types[key], yaml_config[name][key])
+    
+    return yaml_config
 
 
 def load_from_environ():
-    pass
+    environ_config = {}
+    for name, prefix, block in triples:
+        environ_config[name] = {}
+        for key in block._fields:
+            value = environ.get(prefix + key)
+            if value:
+                environ_config[name][key] = cast(block._field_types[key], value)
+    
+    return environ_config
 
 
 def load_from_cli():
     parser = ArgumentParser(description="Webgames Web API for managing games")
+    for name, prefix, block in triples:
+        name_lower = name.lower()
+        for key in block._fields:
+            parser.add_argument("--%s_%s" % (name_lower, key.lower()),
+                                type=real_type(block._field_types[key]),
+                                action="store")
+    return parser.parse_args().__dict__
 
 
-def validate_config(block, yaml_config) -> None:
+def validate_config(name, block, config) -> None:
     """Raise ConfigError for invalids, missings or unknowns options"""
 
-    for key, value in yaml_config.items():
+    for key, value in config.items():
         if key not in block._fields:
-            raise ConfigUnknownOptionError(key, block)
+            raise ConfigUnknownOptionError(key, name)
 
-        if not isinstance(value, block._field_types[key]):
-            raise ConfigOptionTypeError(key, block, type(value))
+        if "Union" in repr(block._field_types[key]):
+            types = block._field_types[key].__args__ 
+        else:
+            types = block._field_types[key]
+        if not isinstance(value, types):
+            raise ConfigOptionTypeError(key, name, type(value))
 
-    missings = set(block._fields) - yaml_config.keys()
+    missings = set(block._fields) - config.keys()
     if missings:
-        raise ConfigMissingOptionError(missings, block)
+        raise ConfigMissingOptionError(missings, name)
+
+
+if __name__ == "__main__":
+    load()
+    for config in [webapi, postgres]:
+        print(config)
--- webapi/exceptions.py
+++ webapi/exceptions.py
@@ -5,17 +5,16 @@ class ConfigOptionTypeError(ConfigError):
     template = "Wrong value type for option \"{}\" in \"{}\" block. " \
                "Type must be {}, current type is {}."
     def __init__(self, option, block, actual_type):
-        super().__init__(self, self.template.format(
+        super().__init__(self.template.format(
             option, block, block._field_types[option], actual_type))
 
 
 class ConfigUnknownOptionError(ConfigError):
     template = "Option \"{}\" in block \"{}\" is unknown."
     def __init__(self, option, block):
-        super().__init__(self, self.template.format(
-            option, block))
+        super().__init__(self.template.format(option, block))
 
 class ConfigMissingOptionError(ConfigError):
-    template = "Options {} from block \"{}\" are missing."
+    template = "Options {{{}}} from block \"{}\" are missing."
     def __init__(self, missings, block):
-        super().__init__(self, self.template.format(missings, block))
+        super().__init__(self.template.format(", ".join(missings), block))
