193218c2dc4a2d6a592de2f6e8d18aa35e9bfee9
Julien00859 <julien.castiaux@gmail.com>
2018-02-11 18:12:03 +0100

    connect to postgres and redis

--- webapi/server.py
+++ webapi/server.py
@@ -1,12 +1,41 @@
 if __name__ == "__main__":
-    # Prevent server.py from beeing imported twice
-    from server import start
-    from sys import exit
-    start()
-    exit(0)
+    raise ImportError("Start the module, not this file.")
 
-def start():
-    import __main__
-    if "exit" in dir(__main__):
-        print("Hack")
-    pass
+import logging
+import aioredis
+import asyncpg
+from sanic import Sanic
+import config
+
+logger = logging.getLogger(__name__)
+app = Sanic()
+
+
+postgres: asyncpg.Connection
+@app.listener("before_server_start")
+async def setup_postgres(app, loop):
+    global postgres
+    postgres = await asyncpg.connect(
+        dsn=config.postgres.DSN,
+        host=config.postgres.HOST,
+        port=config.postgres.PORT,
+        user=config.postgres.USER,
+        database=config.postgres.DATABASE,
+        password=config.postgres.PASSWORD
+    )
+    logger.info("Connection to postgres established.")
+    
+
+redis: aioredis.ConnectionsPool
+@app.listener("before_server_start")
+async def setup_redis(app, loop):
+    global redis
+    if config.redis.DSN is not None:
+        redis = await aioredis.create_pool(address=config.redis.DSN)
+    else:
+        redis = await aioredis.create_pool(
+            address=(config.redis.HOST, config.redis.PORT),
+            db=config.redis.DATABASE,
+            password=config.redis.PASSWORD
+        )
+    logger.info("Connection to redis established.")
