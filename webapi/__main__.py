86ffb6f922e3a4c137aa310417be276796b6638c
Julien00859 <julien.castiaux@gmail.com>
2018-02-11 18:13:35 +0100

    add logging configuration

--- webapi/__main__.py
+++ webapi/__main__.py
@@ -1,2 +1,33 @@
-from server import start
-start()
+import logging
+from logging.handlers import MemoryHandler
+import config
+import server
+
+logger = logging.getLogger(__name__)
+
+# setup logging
+logging.root.level = logging.NOTSET
+logging.addLevelName(45, "SECURITY")
+
+logging.getLogger("sanic.error").handlers.clear()
+logging.getLogger("sanic.access").handlers.clear()
+
+buffer = MemoryHandler(1000000)
+logging.root.addHandler(buffer)
+
+config.load_merge_expose()
+
+stdout = logging.StreamHandler()
+stdout.formatter = logging.Formatter(
+    "{asctime} [{levelname}] <{name}:{funcName}> {message}", style="{")
+stdout.level=logging.INFO
+logging.root.removeHandler(buffer)
+buffer.setTarget(stdout)
+buffer.close()
+logging.root.addHandler(stdout)
+
+logger.debug("debug")
+logger.info("info")
+logger.warning("warning")
+
+# server.app.run(host=config.webapi.HOST, port=config.webapi.PORT)
