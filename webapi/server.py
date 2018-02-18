ce38c1a5841bc2ad4da7585e4f3973e0ea085456
Julien00859 <Julien.castiaux@gmail.com>
2018-02-19 00:46:18 +0100

    safe connect; move exceptions handlers to middlewares

--- webapi/server.py
+++ webapi/server.py
@@ -1,76 +1,61 @@
 """Setup sanic, redis and postgres"""
 
 import logging
+from asyncio import sleep
+from typing import Optional
 import aioredis
 import asyncpg
-from asyncpg.exceptions import IntegrityConstraintViolationError
 from sanic import Sanic
-from sanic.exceptions import SanicException
-from sanic.response import json
+
+app = Sanic(__name__)
 from . import config
+from . import middlewares
 from .routes import auth
 
-
 logger = logging.getLogger(__name__)
-app = Sanic()
 
-
-postgres: asyncpg.Connection
+postgres: Optional[asyncpg.Connection] = None
 @app.listener("before_server_start")
 async def setup_postgres(_, loop):
     """Connect to postgres and expose the connection object"""
     global postgres
-    postgres = await asyncpg.connect(
-        dsn=config.postgres.DSN,
-        host=config.postgres.HOST,
-        port=config.postgres.PORT,
-        user=config.postgres.USER,
-        database=config.postgres.DATABASE,
-        password=config.postgres.PASSWORD,
-        loop=loop
-    )
-    logger.info("Connection to postgres established.")
+    if config.webapi.PRODUCTION:
+        postgres = await asyncpg.connect(
+            dsn=config.postgres.DSN,
+            host=config.postgres.HOST,
+            port=config.postgres.PORT,
+            user=config.postgres.USER,
+            database=config.postgres.DATABASE,
+            password=config.postgres.PASSWORD,
+            loop=loop
+        )
+        logger.info("Connection to postgres established.")
+    else:
+        logger.info("Running without postgres.")
+        await sleep(0)
+    
 
 
-redis: aioredis.ConnectionsPool
+redis: Optional[aioredis.ConnectionsPool] = None
 @app.listener("before_server_start")
 async def setup_redis(_, loop):
     """Connect to redis and expose the connection object"""
     global redis
-    if config.redis.DSN is not None:
-        redis = await aioredis.create_pool(address=config.redis.DSN, loop=loop)
+    if config.webapi.PRODUCTION:
+        if config.redis.DSN is not None:
+            redis = await aioredis.create_pool(
+                address=config.redis.DSN,
+                loop=loop
+            )
+        else:
+            redis = await aioredis.create_pool(
+                address=(config.redis.HOST, config.redis.PORT),
+                db=config.redis.DATABASE,
+                password=config.redis.PASSWORD,
+                loop=loop
+            )
+        logger.info("Connection to redis established.")
     else:
-        redis = await aioredis.create_pool(
-            address=(config.redis.HOST, config.redis.PORT),
-            db=config.redis.DATABASE,
-            password=config.redis.PASSWORD,
-            loop=loop
-        )
-    logger.info("Connection to redis established.")
+        logger.info("Running without redis.")
+        await sleep(0)
 
-
-@app.exception(SanicException)
-def safe_http_error(_, err):
-    """
-    Escape sanic exceptions
-
-    return HTTP status code according to sanic's error with the error
-    contained in the 'error' json field
-    """
-    logger.warning(str(err), exc_info=err)
-    return json({"error": str(err)}, err.status_code)
-
-
-@app.exception(IntegrityConstraintViolationError)
-def safe_sql_error(_, err):
-    """
-    Escape postgres integrity violation
-
-    return HTTP 400 'BadRequest' status code with the error contained in
-    the 'error' field.
-    """
-    logger.warning(str(err), exc_info=err)
-    return json({"error": err.args[0]}, 400)
-
-
-app.blueprint(auth.bp)
