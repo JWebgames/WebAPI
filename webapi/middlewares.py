2ebe1d34ebeedd23b442248d41ec37b2c042bd52
Julien00859 <Julien.castiaux@gmail.com>
2018-02-19 02:36:10 +0100

    sanic workaround

---
+++ webapi/middlewares.py
@@ -0,0 +1,27 @@
+from logging import getLogger
+from asyncpg.exceptions import IntegrityConstraintViolationError
+from sanic.exceptions import SanicException
+from sanic.response import json
+
+logger = getLogger(__name__)
+
+def safe_http(request, exception):
+    """
+    Escape sanic exceptions
+
+    return HTTP status code according to sanic's error with the error
+    contained in the 'error' json field
+    """
+    logger.warning(str(exception), exc_info=exception)
+    return json({"error": str(exception)}, exception.status_code)
+
+
+def safe_sql(request, exception):
+    """
+    Escape postgres integrity violation
+
+    return HTTP 400 'BadRequest' status code with the error contained in
+    the 'error' field.
+    """
+    logger.warning(str(exception), exc_info=exception)
+    return json({"error": exception.args[0]}, 400)
