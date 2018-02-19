f8284a29bd249310b97c9254519f65d97d0812f7
Julien00859 <Julien.castiaux@gmail.com>
2018-02-19 21:40:27 +0100

    add safe_assign

--- webapi/config.py
+++ webapi/config.py
@@ -8,28 +8,41 @@ them in one merged NamedTuple per configuration block
 from argparse import ArgumentParser
 from collections import ChainMap, namedtuple, defaultdict
 from logging import getLogger
-from operator import attrgetter
+from operator import attrgetter, methodcaller
 from os import environ
-from sys import exit
-from typing import NamedTuple, Optional, List
+from sys import argv
+from typing import NamedTuple, Optional, Dict, List, Tuple, Any
 from yaml import safe_load as yaml_load, dump as yaml_dump
 
 from .exceptions import ConfigOptionTypeError,\
                         ConfigUnknownOptionError,\
                         ConfigMissingOptionError
-from .tools import cast, real_type, get_package_path
+from .tools import cast, real_type, get_package_path, find
 
 logger = getLogger(__name__)
 
 
+Triple = namedtuple("Triple", ["name", "prefix", "block"])
+triples = []
+def register(name, prefix=None):
+    def wrapped(block):
+        triples.append(Triple(name, prefix if prefix else name.upper(), block))
+        return block
+    return wrapped
+
+
+webapi: "WebAPIConfig"
+@register("webapi")
 class WebAPIConfig(NamedTuple):
-    HOST: str = "loclhost"
+    HOST: str = "localhost"
     PORT: int = 22548
     JWT_SECRET: str = "super-secret-password"
     LOG_LEVEL: str = "WARNING"
     PRODUCTION: bool = False
 
 
+postgres: "PostgresConfig"
+@register("postgres")
 class PostgresConfig(NamedTuple):
     DSN: Optional[str] = None
     HOST: Optional[str] = None
@@ -39,6 +52,8 @@ class PostgresConfig(NamedTuple):
     PASSWORD: Optional[str] = None
 
 
+redis: "RedisConfig"
+@register("redis")
 class RedisConfig(NamedTuple):
     DSN: Optional[str] = None
     HOST: Optional[str] = None
@@ -47,15 +62,8 @@ class RedisConfig(NamedTuple):
     PASSWORD: Optional[str] = None
 
 
-webapi: WebAPIConfig
-postgres: PostgresConfig
-redis: RedisConfig
-Triple = namedtuple("Triple", ["name", "prefix", "block"])
-triples = [
-    Triple("webapi", "WG_API_", WebAPIConfig),
-    Triple("postgres", "PG", PostgresConfig),
-    Triple("redis", "REDIS_", RedisConfig)
-]
+def safe_assign(source: Dict[str, Dict[str, Any]], block: NamedTuple, blockname: str, field: str, value: Any) -> None:
+    source[blockname][field] = cast(block._field_types[field], value)
 
 
 def get_default():
@@ -65,30 +73,31 @@ def get_default():
 
 def get_from_cli():
     """Get the configuration from the command line"""
-    parser = ArgumentParser(description="Webgames Web API for managing games")
+    sentinel = object()
+    parser = ArgumentParser(description="Webgames Web API for managing games",
+                            argument_default=sentinel)
     for name, _, block in triples:
         name_lower = name.lower()
         for key in block._fields:
             parser.add_argument("--%s_%s" % (name_lower, key.lower()),
                                 type=real_type(block._field_types[key]),
                                 action="store")
-    cli = parser.parse_args()
-    if cli.showconfig:
-        global showconfig
-        showconfig = True
+    cli = parser.parse_args(argv[2:])
 
-    if cli.dryrun:
-        global dryrun
-        dryrun = True
-
-    cliconfig = defaultdict(dict)
     prefixes = list(map(attrgetter("name"), triples))
+    cli_config = defaultdict(dict)
     for key, value in cli.__dict__.items():
-        # TODO
-        pass        
-
+        if value is sentinel:
+            continue
+        pos = key.find("_")
+        if pos < 0 or key[:pos] not in prefixes:
+            continue
+        name = key[:pos]
+        field = key[pos+1:].upper()
+        block = find(lambda fs: fs[0] == name, triples).block
+        safe_assign(cli_config, block, name, field, value)
     
-    return cli.__dict__
+    return cli_config
 
 
 def get_from_env():
@@ -96,10 +105,10 @@ def get_from_env():
     environ_config = {}
     for name, prefix, block in triples:
         environ_config[name] = {}
-        for key in block._fields:
-            value = environ.get(prefix + key)
+        for field in block._fields:
+            value = environ.get(prefix + field)
             if value:
-                environ_config[name][key] = cast(block._field_types[key], value)
+                safe_assign(environ_config, block, name, field, value)
 
     return environ_config
 
@@ -147,7 +156,7 @@ def validate(name: str, block: NamedTuple, config: dict) -> None:
 def merge_sources(*sources) -> List[Tuple[str, NamedTuple]]:
     """Merge and validate all sources against every config block"""
     for name, _, block in triples:
-        return ChainMap(*map(lambda source: source.get(name, {}), sources))
+        merged_config = ChainMap(*map(methodcaller("get", name, {}), sources))
         validate(name, block, merged_config)
         yield name, block(**merged_config)
 
@@ -160,14 +169,14 @@ def expose_block(name: str, block: NamedTuple) -> None:
 def expose_default() -> None:
     """Expose the default configuration"""
     default_config = get_default()
-    for name, _, block in triples:
+    for name, *_ in triples:
         expose_block(name, **default_config[name])
 
 
 def load_merge_validate_expose() -> None:
     """All at once !!! :D"""
     sources = load_all_sources()
-    for name, block in merge_sources():
+    for name, block in merge_sources(*sources):
         expose_block(name, block)
 
 
@@ -181,6 +190,7 @@ def show() -> None:
 
     # Show configuration from each source
     cli, env, yml = load_all_sources()
+    print(cli, env, yml)
     for sourcename, source in [("cli", cli), ("env", env), ("yml", yml)]:
         for blockname, block in source.items():
             print("+{!s:-^63}+".format("{}:{}".format(sourcename, blockname)))
@@ -188,7 +198,7 @@ def show() -> None:
                 print("| {!s:<30}|{!s:>30} |".format(key, value))
 
     # Show merge and validated configuration
-    for name, block in merge_all_sources(cli, env, yml):
+    for name, block in merge_sources(cli, env, yml):
         print("+{:-^63}+".format("Merged:" + name))
         for key, value in block._asdict().items():
             print("| {!s:<30}|{!s:>30} |".format(key, value))
