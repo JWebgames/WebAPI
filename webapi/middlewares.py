37e1dd2339365f5ca3b3b1d1c5be61e4fd82bb57
Julien00859 <julien.castiaux@gmail.com>
2018-02-25 16:40:58 +0100

    add authenticate & require_field mw + test runner

--- webapi/middlewares.py
+++ webapi/middlewares.py
@@ -5,15 +5,16 @@ from enum import Enum
 from functools import wraps
 from ipaddress import ip_address
 from logging import getLogger
+from sqlite3 import IntegrityError as SQLiteIntegrityError
 import jwt as jwtlib
-from asyncpg.exceptions import IntegrityConstraintViolationError
+from asyncpg.exceptions import IntegrityConstraintViolationError as PostgresIntegrityError
 from sanic.exceptions import SanicException,\
                              InvalidUsage,\
                              Unauthorized,\
                              Forbidden
 from sanic.response import json
+from . import database
 from .config import webapi
-from .databases import KVS
 from .server import app
 
 logger = getLogger(__name__)
@@ -27,19 +28,21 @@ class ClientType(Enum):
     MANAGER = "manager"
 
 @app.middleware("request")
-def get_real_ip(req):
+def set_real_ip(req):
+    """Replace the actual IP with the real IP of the client"""
+    if webapi.REVERSE_PROXY_IPS is None:
+        return
+
     if ip_address(req.ip) in webapi.REVERSE_PROXY_IPS:
-        xff = req.headers.get("X-Forwarded-For")
-        if xff is not None:
-            req.ip = ip_address(xff.split(", ", 1)[0])
+        header_xff = req.headers.get("X-Forwarded-For")
+        if header_xff is not None:
+            req.ip = eader_xff.split(", ", 1)[0]
             return
         
-        xri = req.headers.get("X-Real-IP")
-        if xri is not None:
-            req.ip = ip_address(xri)
+        header_xri = req.headers.get("X-Real-IP")
+        if header_xri is not None:
+            req.ip = header_xri
             return
-    
-    req.ip = ip_address(req.ip)
 
 
 @app.exception(SanicException)
@@ -54,7 +57,7 @@ def safe_http(request, exception):
     return json({"error": str(exception)}, exception.status_code)
 
 
-@app.exception(IntegrityConstraintViolationError)
+@app.exception(SQLiteIntegrityError, PostgresIntegrityError)
 def safe_sql(request, exception):
     """
     Escape postgres integrity violation
@@ -73,11 +76,11 @@ def authenticate(allowed_client_types: set):
             bearer = req.headers.get("Authorization")
             if not bearer:
                 logger.warning(f"Authorization header is missing (IP: {req.ip})")
-                raise Unauthorized("Authorization header required", "Bearer")
+                raise Unauthorized("Authorization header required")
 
             if not bearer.startswith("Bearer:"):
                 logger.warning(f"Wrong authorization header type (IP: {req.ip})")
-                raise Unauthorized("Bearer authorization type required", "Bearer")
+                raise Unauthorized("Bearer authorization type required")
 
             try:
                 jwt = jwtlib.decode(bearer[7:].strip(), webapi.JWT_SECRET)
@@ -85,23 +88,23 @@ def authenticate(allowed_client_types: set):
                 logger.log(45, f"Invalid token (IP: {req.ip})")
                 raise Forbidden("Invalid token")
 
-            if KVS.is_token_revoked(jwt["token_id"]):
-                logger.log(45, f"Token has been removed (IP: {req.ip})")
-                raise Forbidden("Invalid token")
-
+            if await database.KVS.is_token_revoked(jwt["tid"]):
+                logger.log(45, f"Token has been revoked (IP: {req.ip})")
+                raise Forbidden("Revoked token")
             
-            if ClientType(jwt["type"]) not in allowed_types:
-                logger.log(45, f"Invalid token type: \"{jwt['type']}\" not in {allowed_types} (IP: {req.ip})")
-                raise Forbidden("Invalid token type")
+            if ClientType(jwt["typ"]) not in allowed_client_types:
+                logger.log(45, f"Restricted access: \"{jwt['typ']}\" not in {allowed_client_types} (IP: {req.ip})")
+                raise Forbidden("Restricted access")
 
             return await func(req, *args, **kwargs, jwt=jwt)
         return authenticate_wrapped
     return authenticate_wrapper
 
+
 def require_fields(fields: set):
-    def wrapper(func):
+    def require_fields_wrapper(func):
         @wraps(func)
-        async def wrapped(req, *args, **kwargs):
+        async def require_fields_wrapped(req, *args, **kwargs):
             ct = req.headers.get("Content-Type")
             if ct is None or "application/json" not in ct:
                 raise InvalidUsage("JSON required")
@@ -114,6 +117,6 @@ def require_fields(fields: set):
             missings = fields - req.json.keys()
             if missings:
                 raise InvalidUsage(f"Fields {missings} are missing")
-            return await func(req, *args, **kwargs)
-        return wrapped
-    return wrapper
+            return await func(req, *args, **req.json, **kwargs)
+        return require_fields_wrapped
+    return require_fields_wrapper
