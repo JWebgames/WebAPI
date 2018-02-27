e74c030aec0de62f7ad32d39e06e436bc0346ac6
Julien <Julien.castiaux@gmail.com>
2018-02-27 01:18:50 +0100

    build

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='webapi',
-      version='0.1',
-      description="Webgames Web API for managing games",
-      url="https://git.julien00859.be/Webgames/WebAPI",
-      author="Julien Castiaux",
-      author_email="Julien.castiaux@gmail.com",
-      license="MIT",
-      packages=["webapi"],
-      install_requires=[
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='julien-webapi',
+    version='0.1.0',
+    description='Webgames Web REST API',
+    long_description=long_description,
+    url='https://github.com/JWebgames/webapi',
+    author='Julien Castiaux',
+    author_email='Julien.castiaux@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Framework :: AsyncIO',
+        'Framework :: Sanic',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Games/Entertainment',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
           "aioredis",
           "asyncpg",
           "pyjwt",
@@ -16,5 +35,8 @@ setup(name='webapi',
           "PyYAML",
           "sanic",
           "scrypt"
-      ],
-      zip_safe=False)
+    ],
+    extras_require={
+        'test': ['pylint', 'coverage', 'codecov', 'aiohttp'],
+    },
+)
