8a538c02c52b10c58bca9e0ec1c5f1886b2ee40e
Julien00859 <julien.castiaux@gmail.com>
2018-04-16 00:14:55 +0200

    add capacity

--- webapi/storage/sql_queries/sqlite/create_table_games.sql
+++ webapi/storage/sql_queries/sqlite/create_table_games.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS tbgames (
     gameid INTEGER NOT NULL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
-    ownerid BLOB NOT NULL,
+    ownerid TEXT NOT NULL,
+    capacity INTEGER NOT NULL,
 
     CONSTRAINT fk_ownerid FOREIGN KEY (ownerid)
         REFERENCES tbusers
