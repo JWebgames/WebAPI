13e5d14053659865edbe60d5cfb53a04f0d685fd
Julien <Julien.castiaux@gmail.com>
2018-05-01 20:54:27 +0200

    environ to select level of tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,18 @@
 """Configure logging and load default configuration"""
 
 import logging
-from webapi.config import expose_default
+from os import getenv
+from distutils.util import strtobool
+import webapi.config
 
 #logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(filename="test.log")
+logging.basicConfig(filename="test.log", filemode="w")
 logging.addLevelName(45, "SECURITY")
 
-#expose_default()
-expose_default(webapi={"PRODUCTION": True})
+
+if strtobool(getenv("WEBAPI_TEST_USING_CONFIG", "no")):
+    webapi.config.load_merge_validate_expose()
+elif strtobool(getenv("WEBAPI_PRODUCTION", "no")):
+    webapi.config.expose_default(webapi={"PRODUCTION": True})
+else:
+    webapi.config.expose_default()
