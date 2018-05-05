6a1855b933a95302c8fa272b22fa396a172340e3
Julien00859 <julien.castiaux@gmail.com>
2018-05-05 17:40:36 +0200

    set log verbosity to debug

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,13 +6,13 @@ from distutils.util import strtobool
 import webapi.config
 
 #logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(filename="test.log", filemode="w")
+logging.basicConfig(filename="test.log", filemode="w", level=logging.DEBUG)
 logging.addLevelName(45, "SECURITY")
 
 
 if strtobool(getenv("WEBAPI_TEST_USING_CONFIG", "no")):
     webapi.config.load_merge_validate_expose()
 elif strtobool(getenv("WEBAPI_PRODUCTION", "no")):
-    webapi.config.expose_default(webapi={"PRODUCTION": True})
+    webapi.config.expose_default(webapi={"PRODUCTION": True, "LOG_LEVEL": logging.DEBUG})
 else:
     webapi.config.expose_default()
