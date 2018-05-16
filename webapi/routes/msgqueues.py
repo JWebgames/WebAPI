86aa432e514be3cad9470f834cc7708888e9fc21
Julien00859 <julien.castiaux@gmail.com>
2018-05-16 02:54:28 +0200

    debug

--- webapi/routes/msgqueues.py
+++ webapi/routes/msgqueues.py
@@ -1,3 +1,4 @@
+from asyncio import ensure_future
 from logging import getLogger
 from operator import methodcaller
 from sanic import Blueprint
@@ -12,19 +13,21 @@ bp = Blueprint("msgqueues")
 logger = getLogger(__name__)
 
 async def feeder(res, queue):
-    chan = await drivers.KVS.redis.subscribe(queue)
+    chan = (await drivers.KVS.redis.subscribe(queue))[0]
     while await chan.wait_message():
-        event = await chan.get(encoding="utf-8")
-        res.write(event + chr(30))  # ascii unit separator
+        msg = await chan.get(encoding="utf-8")
+        logger.debug("Send message \"%s\" from queue %s to %s", queue, msg, res)
+        res.write(msg)
+        res.write(chr(30))  # ascii unit separator
 
 @bp.route("/user", methods=["GET"])
 @authenticate({ClientType.PLAYER, ClientType.ADMIN})
 async def get_user_msg(req, jwt):
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.USER, jwt["uid"])
+    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.USER.value, jwt["uid"])
     async def recv(res):
         await feeder(res, queue)
     
-    await drivers.KVS.send_message(MsgQueueType.USER, jwt["uid"], {"type": "server:notice", "notice": "hello"})
+    ensure_future(drivers.KVS.send_message(MsgQueueType.USER.value, jwt["uid"], {"type": "server:notice", "notice": "hello"}))
     return stream(recv)
     
 
@@ -35,7 +38,7 @@ async def get_group_msg(req, jwt):
     if user.groupid is None:
         raise InvalidUsage() from PlayerNotInGroup
 
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.GROUP, user.groupid)
+    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.GROUP.value, user.groupid)
     async def recv(res):
         await feeder(res, queue)
     return stream(recv)
@@ -47,7 +50,7 @@ async def get_party_msg(req, jwt):
     if user.partyid is None:
         raise InvalidUsage() from PlayerNotInParty()
 
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.PARTY, user.groupid)
+    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.PARTY.value, user.groupid)
     async def recv(res):
         await feeder(res, queue)
     return stream(recv)
