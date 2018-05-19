70880112c573918bba899dff1c9d2930561b4393
Julien00859 <julien.castiaux@gmail.com>
2018-05-19 21:39:10 +0200

    use new implementation of msgqueues

--- webapi/routes/msgqueues.py
+++ webapi/routes/msgqueues.py
@@ -1,34 +1,77 @@
-from asyncio import ensure_future
+import asyncio
+from contextlib import suppress
 from logging import getLogger
 from operator import methodcaller
+from collections import defaultdict
 from sanic import Blueprint
 from sanic.response import stream
 from sanic.exceptions import InvalidUsage
+from ..server import app
 from ..exceptions import PlayerNotInParty, PlayerNotInGroup
 from ..middlewares import authenticate, require_fields
 from ..storage import drivers
 from ..storage.models import ClientType, MsgQueueType
+from ..tools import async_partial
 
 bp = Blueprint("msgqueues")
 logger = getLogger(__name__)
+tasks = []
+stop_events = []
 
-async def feeder(res, queue):
-    chan = (await drivers.KVS.redis.subscribe(queue))[0]
-    while await chan.wait_message():
-        msg = await chan.get(encoding="utf-8")
-        logger.debug("Send message \"%s\" from queue %s to %s", queue, msg, res)
-        res.write(msg)
-        res.write(chr(30))  # ascii unit separator
+async def heartbeat(res, stop_event):
+    logger.info("Start beating on %s", res)
+    with suppress(asyncio.CancelledError):
+        while True:
+            await asyncio.sleep(30)
+            if res.transport.is_closing():
+                logger.debug("Transport %s closed", res.transport)
+                stop_event.set()
+                break
+            res.write('{"type":"heartbeat"}')
+            res.write(chr(30))  # ascii unit separator
+    logger.info("Stop beating on %s", res)
+
+
+async def sub_proxy(res, stop_event, queue, id_):
+    logger.info("%s subscribed to queue %s:%s", res, queue, id_)
+    with suppress(asyncio.CancelledError):
+        for msg in drivers.KVS.recv_messages(queue, id_):
+            if res.transport.is_closing():
+                logger.debug("Transport %s closed", res.transport)
+                stop_event.set()
+                break
+            logger.debug("Send message %s from queue %s:%s to %s",
+                         msg, queue, id_, res)
+            res.write(msg)
+            res.write(chr(30))  # ascii unit separator
+    logger.info("%s's subscribtion to queue %s ended", res, queue)
+
+
+async def stream_until_event_is_set(res, func):
+    stop_event = asyncio.Event()
+    func_task = asyncio.ensure_future(func(res, stop_event))
+    hb_task = asyncio.ensure_future(heartbeat(res, stop_event))
+
+    stop_events.append(stop_event)
+    tasks.extend([func_task, hb_task])
+
+    with suppress(asyncio.CancelledError):
+        await stop_event.wait()
+    func_task.cancel()
+    hb_task.cancel()
+
+    stop_events.remove(stop_event)
+    tasks.remove(func_task)
+    tasks.remove(hb_task)
+
 
 @bp.route("/user", methods=["GET"])
 @authenticate({ClientType.PLAYER, ClientType.ADMIN})
 async def get_user_msg(req, jwt):
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.USER.value, jwt["uid"])
-    async def recv(res):
-        await feeder(res, queue)
-    
-    ensure_future(drivers.KVS.send_message(MsgQueueType.USER.value, jwt["uid"], {"type": "server:notice", "notice": "hello"}))
-    return stream(recv)
+    greetings(MsgQueueType.USER.value, user["jwt"])
+    return stream(async_partial(stream_until_event_is_set,
+        func=async_partial(sub_proxy(queue=MsgQueueType.PLAYER.value,
+                                     id_=jwt["uid"]))))
     
 
 @bp.route("/group", methods=["GET"])
@@ -36,21 +79,34 @@ async def get_user_msg(req, jwt):
 async def get_group_msg(req, jwt):
     user = await drivers.KVS.get_user(jwt["uid"])
     if user.groupid is None:
-        raise InvalidUsage() from PlayerNotInGroup
+        raise PlayerNotInGroup()
 
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.GROUP.value, user.groupid)
-    async def recv(res):
-        await feeder(res, queue)
-    return stream(recv)
+    greetings(MsgQueueType.PARTY.value, user.partyid)
+    return stream(async_partial(stream_until_event_is_set,
+        func=async_partial(sub_proxy(queue=MsgQueueType.GROUP.value,
+                                     id_=user.groupid))))
 
 @bp.route("/party", methods=["GET"])
 @authenticate({ClientType.PLAYER, ClientType.ADMIN})
 async def get_party_msg(req, jwt):
     user = await drivers.KVS.get_user(jwt["uid"])
     if user.partyid is None:
-        raise InvalidUsage() from PlayerNotInParty()
+        raise PlayerNotInParty()
 
-    queue = drivers.Redis.msgqueue_key.format(MsgQueueType.PARTY.value, user.groupid)
-    async def recv(res):
-        await feeder(res, queue)
-    return stream(recv)
+    greetings(MsgQueueType.GROUP.value, user.groupid)
+    subfunc = 
+    return stream(async_partial(stream_until_event_is_set,
+        func=async_partial(sub_proxy(queue=MsgQueueType.PARTY.value,
+                                     id_=user.partyid))))
+
+def greetings(queue, id_):
+    payload = {"type":"server:notice","notice":"hello %d" % queue.value}
+    coro = drivers.KVS.send_message(queue, id_, payload)
+    asyncio.get_event_loop().call_later(0.2, asyncio.ensure_future, coro)
+
+async def close_all_connections(_app, _loop):
+    logger.info("Closing all streaming connections...")
+    for task in tasks.copy():
+        task.cancel()
+    for event in stop_events.copy():
+        event.set()
